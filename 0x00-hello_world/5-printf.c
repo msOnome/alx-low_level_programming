287a553b23077d5316bca5522506994b8756e8c3
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-13 16:18:13 -0700

    Adds task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Starting point of execution
+ *
+ *Return: success, terminates program
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
