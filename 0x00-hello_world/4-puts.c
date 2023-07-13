15229f835ffcdf47ad61ae4aa9ce67ba4ce30ece
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-13 15:49:45 -0700

    Updates task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-echo '#include <stdio.h> int main() { puts("Programming is like building a multilingual puzzle"); return 0; }' | gcc -xc - && ./a.out
+#include <stdio.h>
+
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
