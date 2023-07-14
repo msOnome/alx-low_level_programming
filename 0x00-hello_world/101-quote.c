bec1be23e79d0c79c04b4b74301298a4fd5194e4
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-14 12:20:55 -0700

    Adds 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  *
  * Return: Success, Program terminates
  */
-int main(void 
+int main(void) 
 {
 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, strlen(message));
