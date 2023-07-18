25ff60dca3508c1ddedf18f80d03c86d25473be1
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-17 17:30:55 -0700

    Updates task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 /**
  * main - Program starts
  *
- * Return: Success, Program terminates
+ * Return: Return 1, Success
  */
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, "
-"2015-10-19\n";
-write(2, message, strlen(message));
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
