83a9186476e4f76308e6035d14c6740c1b6507f0
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-19 15:04:12 -0700

    Adds main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include main.h
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alphabets;
+	for (alphabet = a; alphabet <= z; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
+}
