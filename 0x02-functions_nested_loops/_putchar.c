4d59bc24117391617a77eceab4e4a6c33508deca
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-19 15:32:27 -0700

    Clean up

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * Return: 1 on success and -1 on error
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
