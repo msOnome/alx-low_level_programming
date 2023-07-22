cb5d14ea95706afcef9a26eb2f0296a3fea0e938
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-21 17:19:34 -0700

    clean up on task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - checks if an alphabet is uppercase
- * @x - The alphabet is to be checked
+ * _isupper - Checks if a letter is uppercase
+ * @x - The letter is to be checked
  * Return : returns 1 for uppercase and 0 for lowercase
  */
 
@@ -10,7 +10,7 @@ int _isupper(int x)
 {
 	if(x >= 65 && x <= 90)
 	{
-        return (1);
+		return (1);
 	}
-	return (0);
+        	return (0);
 }
