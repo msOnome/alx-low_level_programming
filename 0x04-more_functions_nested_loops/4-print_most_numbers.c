239528cde99bc2363bc2d276f6cf7a627ed79298
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-21 18:43:36 -0700

    Adds task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9
+ * Description: skips the numbers 2 and 4
+ * Return: The numbers 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+	if (i = 2 || i = 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(i + '0');
+	}
+	}
+	_putchar('\n')
+}
