9ab33de6b21e5a57a8c9e6f2b3d432b041f8ac78
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-21 19:24:04 -0700

    Adds tasks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print the numbers from 0 to 9
- * Return: returns the numbers from 0 to 9
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
 {
-        int i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
-        {
-        _putchar(i + '0');
-        }
-        _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
