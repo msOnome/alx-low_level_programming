388c3cf263a0de98e3446258e2c007798ef3a236
Ofogba Ofogba <ofogbaonome@gmail.com>
2023-07-21 17:54:52 -0700

    adds task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ * Return: the multiplied value of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
